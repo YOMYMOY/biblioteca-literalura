e64f4a381cedb4c15008f56c9da5b2cfeb9a55e1
Matias - YOMYMOY <matdnunez@gmail.com>
2025-01-11 14:56:55 -0300

    primer commit

---
+++ src/main/java/com/yomymoy/biblioteca_literalura/BibliotecaLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.yomymoy.biblioteca_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaLiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yomymoy/biblioteca_literalura/BibliotecaLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yomymoy.biblioteca_literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
